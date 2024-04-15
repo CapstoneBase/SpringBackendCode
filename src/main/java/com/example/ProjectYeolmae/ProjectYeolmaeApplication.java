a267b02b6e7fa34c2c891797d97e5edd9c87db32
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-04-16 00:30:39 +0900

    first commit

---
+++ src/main/java/com/example/ProjectYeolmae/ProjectYeolmaeApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectYeolmae;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectYeolmaeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectYeolmaeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProjectYeolmae/ProjectYeolmaeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProjectYeolmae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectYeolmaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
