4c6d788bc38ee70dfee9de92c8ad6db8a79532f1
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-05-05 16:11:37 +0900

    [init]

---
+++ src/main/java/com/capstone/yeolmaeTeamProject/YeolmaeTeamProjectApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.yeolmaeTeamProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YeolmaeTeamProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YeolmaeTeamProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/capstone/yeolmaeTeamProject/YeolmaeTeamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capstone.yeolmaeTeamProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YeolmaeTeamProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
