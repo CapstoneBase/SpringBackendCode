4074acb440375e4ef5ddef25eac17883090d7f67
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-05-07 00:41:20 +0900

    feat: JWT의 authenticationfilter 추가

--- src/main/java/com/capstone/yeolmaeTeamProject/global/config/SecurityConfig.java
+++ src/main/java/com/capstone/yeolmaeTeamProject/global/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.capstone.yeolmaeTeamProject.global.config;
 
+import com.capstone.yeolmaeTeamProject.global.auth.filter.JwtAuthenticationFilter;
+import com.capstone.yeolmaeTeamProject.global.auth.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,10 +11,14 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -19,6 +26,10 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(
+                        new JwtAuthenticationFilter(jwtTokenProvider),
+                        UsernamePasswordAuthenticationFilter.class
                 )
                 .authorizeRequests(this::configureRequests);
         return http.build();
