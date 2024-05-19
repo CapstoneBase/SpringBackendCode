88dba505bb387ce2dbb7433369208be724038fed
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-05-19 22:56:03 +0900

    feat: 게시판 entity 설정 및 추가

---
+++ src/main/java/com/capstone/yeolmaeTeamProject/domain/community/domain/Post.java
@@ -0,0 +1,41 @@
+package com.capstone.yeolmaeTeamProject.domain.community.domain;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(indexes = {
+        @Index(name = "idx_category", columnList = "category"),
+        @Index(name = "idx_parentCategory", columnList = "parentCategory")
+})
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    //
+    @Column(nullable = false)
+    private String category;
+
+    private String parentCategory;
+
+    @Column(nullable = false)
+    @Size(min = 1, max = 100, message = "제목은 1자 이상 100자 이하로 입력해주세요.")
+    private String title;
+
+    @Column(nullable = false)
+    @Size(min = 1, max = 10000, message = "내용은 1자 이상 1000자 이하로 입력해주세요.")
+    private String content;
+
+    @Column(nullable = false)
+    private String writer;
+
+
+}
