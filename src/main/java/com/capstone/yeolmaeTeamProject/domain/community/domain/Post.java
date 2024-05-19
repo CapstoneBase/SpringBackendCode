a2cc1f9e4cff9c2998060dfdc3da1bf06699cf6f
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-05-19 23:10:36 +0900

    refactoring: 게시판 entity 수정 및 1:N 관계 추가

--- src/main/java/com/capstone/yeolmaeTeamProject/domain/community/domain/Post.java
+++ src/main/java/com/capstone/yeolmaeTeamProject/domain/community/domain/Post.java
@@ -1,7 +1,11 @@
 package com.capstone.yeolmaeTeamProject.domain.community.domain;
 
+import com.capstone.yeolmaeTeamProject.domain.user.domain.User;
+import com.capstone.yeolmaeTeamProject.global.common.domain.BaseEntity;
+
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Size;
+
 import lombok.*;
 
 @Entity
@@ -14,13 +18,12 @@ import lombok.*;
         @Index(name = "idx_category", columnList = "category"),
         @Index(name = "idx_parentCategory", columnList = "parentCategory")
 })
-public class Post {
+public class Post extends BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //
     @Column(nullable = false)
     private String category;
 
@@ -34,8 +37,9 @@ public class Post {
     @Size(min = 1, max = 10000, message = "내용은 1자 이상 1000자 이하로 입력해주세요.")
     private String content;
 
-    @Column(nullable = false)
-    private String writer;
+    @ManyToOne
+    @JoinColumn(name = "writer_id", nullable = false)
+    private User writer;
 
-
+    private String imageUrl;
 }
