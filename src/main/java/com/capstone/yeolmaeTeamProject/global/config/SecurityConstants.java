8b6347d4e8bafcc34830b8ab6fae97d82467d2c4
dbsgh4937 <130148224+dbsgh4937@users.noreply.github.com>
2024-05-25 18:50:46 +0900

    fix: 기능 API 잘못된 권한 요구 에러 수정

--- src/main/java/com/capstone/yeolmaeTeamProject/global/config/SecurityConstants.java
+++ src/main/java/com/capstone/yeolmaeTeamProject/global/config/SecurityConstants.java
@@ -17,7 +17,9 @@ public class SecurityConstants {
     };
 
     public static final String[] PERMIT_ALL_API_ENDPOINTS_POST = {
-            "/api/v1/login/**",
-            "/api/v1/users/check",
+            "/api/v1/login",
+            "/api/v1/login/reissue",
+            "/api/v1/users",
+            "/api/v1/users/check"
     };
 }
